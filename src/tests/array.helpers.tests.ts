0e16f8062ce85c3f9d3a9f3ed94770b7de676c72
Eduardo Juliao <eduardo.juliaojr@hotmail.com>
2020-02-18 16:10:29 +0000

    tests

--- src/tests/array.helpers.tests.ts
+++ src/tests/array.helpers.tests.ts
@@ -1,1 +1,12 @@
 import { diff } from '../lib/helpers/array.helpers';
+import * as assert from 'assert';
+
+describe('Array helper tests', () => {
+   it('can get different items in two array', () => {
+      const source = ["0", "1", "2", "3", "4", "5"];
+      const compare = ["0", "2", "4"];
+      const expected = ["1", "3", "5"];
+      const difference = diff(source, compare);
+      assert.deepEqual(difference, expected);
+   })
+})
